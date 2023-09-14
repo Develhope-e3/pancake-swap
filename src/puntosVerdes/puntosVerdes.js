88296046cdfd4f521c9783e87ab203ad0b21f015
Barbara Contardi <barbi.contardi@gmail.com>
2023-09-14 11:53:44 +0200

    chore:hice las correcciones

--- src/puntosVerdes/puntosVerdes.js
+++ src/puntosVerdes/puntosVerdes.js
@@ -3,28 +3,12 @@ import "./puntosVerdes.scss";
 /**
  *
  * @param {string} className - Clase que va a llevar el boton
- * 
+ *
  * @returns Estructura HTML
  */
 
-const PuntosVerdes = ({className }) => {
-
-  /**
-   * Logica de la aplicacion
-   */
-
-
-  /**
-   * HTML a retornar
-   */
-  return (
-    <div className={className}>
-      
-    </div>
-  );
+const PuntosVerdes = ({ className }) => {
+  return <div className={className}></div>;
 };
 
-/**
- * Siempre va al final del componente, para poder importarlo en mi aplicacion
- */
 export default PuntosVerdes;
