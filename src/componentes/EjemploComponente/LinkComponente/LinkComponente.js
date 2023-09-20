85de90f80fd37e0c94a8c78f0448937eaf0ad22c
Moguete <esteveptt@gmail.com>
2023-09-20 17:47:16 +0200

    feat/ liandola con el color

--- src/componentes/EjemploComponente/LinkComponente/LinkComponente.js
+++ src/componentes/EjemploComponente/LinkComponente/LinkComponente.js
@@ -2,17 +2,18 @@ import "./LinkComponente.scss";
 
 /**
  * @param {string} label - Texto que va a llevar el link
- * @param {string} className - clase del link
+ * @param {string} className - clase del link (primaryAnchor, secondaryAnchor, tertiaryAnchor)
  * @param {string} href - URL del link
  * @param {object} svg - imagen SVG del link
+ * @param {string} color - variable del color del link
  *
  * @returns Estructura HTML
  */
 
-const LinkComponente = ({ label, className, href, svg }) => {
+const LinkComponente = ({ label, className, href, svg, color }) => {
   return (
-    <a class={className} href={href}>
-      {label} {svg}
+    <a class={className} style={color var({color})} href={href}>
+      {label} {svg && svg}
     </a>
   );
 };
