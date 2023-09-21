982069ef8bd570113a6ce6b73d93687b7d84a606
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-09-21 17:28:29 +0200

    feat: add more typography on Texto element

--- src/componentes/Texto/Texto.js
+++ src/componentes/Texto/Texto.js
@@ -13,6 +13,41 @@ const Texto = ({ size, color, label }) => {
           {label}
         </h1>
       )}
+      {size === "title-medium-bold" && (
+        <h2 className="title-md-bold" style={{ color: `var(${color})` }}>
+          {label}
+        </h2>
+      )}
+      {size === "title-medium" && (
+        <h2 className="title-md" style={{ color: `var(${color})` }}>
+          {label}
+        </h2>
+      )}
+      {size === "title-small-bold" && (
+        <h3 className="title-s-bold" style={{ color: `var(${color})` }}>
+          {label}
+        </h3>
+      )}
+      {size === "title-small" && (
+        <h3 className="title-s" style={{ color: `var(${color})` }}>
+          {label}
+        </h3>
+      )}
+      {size === "title-extra-small-bold" && (
+        <h4 className="title-xs-bold" style={{ color: `var(${color})` }}>
+          {label}
+        </h4>
+      )}
+      {size === "title-extra-small" && (
+        <h4 className="title-xs" style={{ color: `var(${color})` }}>
+          {label}
+        </h4>
+      )}
+      {size === "body" && (
+        <p className="body" style={{ color: `var(${color})` }}>
+          {label}
+        </p>
+      )}
       {size === "medium" && <h2>{label}</h2>}
       {size === "small" && <h3>{label}</h3>}
       {size === "extra-small" && <h4>{label}</h4>}
