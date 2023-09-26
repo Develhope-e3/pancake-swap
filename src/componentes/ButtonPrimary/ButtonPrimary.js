3cb12cd9225ec09f3d9420f0f65bc67c96b2b231
Marcoski <marcosdediego@gmail.com>
2023-09-26 14:44:33 +0200

    correccion-buttons

--- src/componentes/ButtonPrimary/ButtonPrimary.js
+++ src/componentes/ButtonPrimary/ButtonPrimary.js
@@ -4,21 +4,17 @@ import "./ButtonPrimary.scss";
 /**
  *
  * @param {string} texto - Texto que va a llevar el boton
- * @param {Boolean} esPrimario - Boolean para indicar si el boton es primario o secundario
- * @param {Boolean} disabled - Boolean para indicar si el boton esta desactivado
+ * @param {string} className - texto para indicar la ClassName para adjuntar en css
  * @param {Callback} onClick - Callback que se ejecuta cada vez que el usuario hace click en el boton
- * @param {JSX.Element} svg - Elemento SVG o Icono de react-icons
- *
  * @returns Estructura HTML
  */
 
-export const ButtonPrimary = ({ texto, onClick, svg, className }) => {
+export const ButtonPrimary = ({ texto, onClick, className }) => {
 //   const className = esPrimario ? "primario" : "secundario";
 
   return (
     <button className={className} onclick={onClick}>
       {texto}
-      {svg && svg}
     </button>
   );
 };
--- src/componentes/ButtonSecondary/ButtonSecondary.js
+++ src/componentes/ButtonSecondary/ButtonSecondary.js
@@ -4,10 +4,8 @@ import "./ButtonSecondary.scss";
 /**
  *
  * @param {string} texto - Texto que va a llevar el boton
- * @param {Boolean} esPrimario - Boolean para indicar si el boton es primario o secundario
- * @param {Boolean} disabled - Boolean para indicar si el boton esta desactivado
+ * @param {string} ClassName - texto para indicar la ClassName para adjuntar en css
  * @param {Callback} onClick - Callback que se ejecuta cada vez que el usuario hace click en el boton
- * @param {JSX.Element} svg - Elemento SVG o Icono de react-icons
  *
  * @returns Estructura HTML
  */
