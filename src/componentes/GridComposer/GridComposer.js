926fcadf22a4a2ee00a63e5c2c9101d55ad2126f
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-09-30 16:20:07 +0200

    refactor: GridComposer

--- src/componentes/GridComposer/GridComposer.js
+++ src/componentes/GridComposer/GridComposer.js
@@ -28,100 +28,88 @@ export const GridComposer1 = ({
           gridColumnGap: `${remColSpacing}rem`,
         }}>
         <div>
-          <span>
+          <>
             <Texto
               size={"title-s"}
               color={"--color-subtitulo-lila"}
               label={"Circulating Supply"}
             />
-          </span>
-          <p>
             <Texto
               size={"title-ml"}
               color={"--text-color"}
               label={<CakeSum limite={22456392} />}
             />
-          </p>
+          </>
         </div>
         <div>
-          <span>
+          <>
             <Texto
               size={"title-s"}
               color={"--color-subtitulo-lila"}
               label={"Total Supply"}
             />
-          </span>
-          <p>
             <Texto
               size={"title-ml"}
               color={"--text-color"}
               label={<CakeSum limite={398513365} />}
             />
-          </p>
+          </>
         </div>
         <div>
-          <span>
+          <>
             <Texto
               size={"title-s"}
               color={"--color-subtitulo-lila"}
               label={"Max Supply"}
             />
-          </span>
-          <p>
             <Texto
               size={"title-ml"}
               color={"--text-color"}
               label={<CakeSum limite={750000000} />}
             />
-          </p>
+          </>
         </div>
         <div>
-          <span>
+          <>
             <Texto
               size={"title-s"}
               color={"--color-subtitulo-lila"}
               label={"Market Cap"}
             />
-          </span>
-          <p>
             <Texto
               size={"title-ml"}
               color={"--text-color"}
               label={"$280 million"}
             />
-          </p>
+          </>
         </div>
         <div>
-          <span>
+          <>
             <Texto
               size={"title-s"}
               color={"--color-subtitulo-lila"}
               label={"Burned to date"}
             />
-          </span>
-          <p>
             <Texto
               size={"title-ml"}
               color={"--text-color"}
-              label={"$280 million"}
+              label={<CakeSum limite={952660681} />}
             />
-          </p>
+          </>
         </div>
         <div>
-          <span>
+          <>
             <Texto
               size={"title-s"}
               color={"--color-subtitulo-lila"}
               label={"Current Emissions"}
             />
-          </span>
-          <p>
             <Texto
               size={"title-ml"}
               color={"--text-color"}
               label={"$2,93/block"}
             />
-          </p>
+          </>
         </div>
       </div>
     </div>
@@ -145,84 +133,74 @@ export const GridComposer2 = ({
           gridColumnGap: `${remColSpacing}rem`,
         }}>
         <div className="border-none">
-          <span>
+          <>
             <Texto
               size={"title-s"}
               color={"--color-text"}
               label={"Circulating Supply"}
             />
-          </span>
-          <p>
             <Texto
               size={"title-ml"}
               color={"--color-titulo-lila"}
               label={<CakeSum limite={227026415} />}
             />
-          </p>
+          </>
         </div>
         <div>
-          <span>
+          <>
             <Texto
               size={"title-s"}
               color={"--color-text"}
               label={"Total Supply"}
             />
-          </span>
-          <p>
             <Texto
               size={"title-ml"}
               color={"--color-titulo-lila"}
               label={<CakeSum limite={398171116} />}
             />
-          </p>
+          </>
         </div>
         <div>
-          <span>
+          <>
             <Texto
               size={"title-s"}
               color={"--color-text"}
               label={"Market Cap"}
             />
-          </span>
-          <p>
             <Texto
               size={"title-ml"}
               color={"--color-titulo-lila"}
               label={"$260 million"}
             />
-          </p>
+          </>
         </div>
         <div>
-          <span>
+          <>
             <Texto
               size={"title-s"}
               color={"--color-text"}
               label={"Token Burn"}
             />
-          </span>
-          <p>
             <Texto
               size={"title-ml"}
               color={"--color-titulo-lila"}
               label={<CakeSum limite={987196236} />}
             />
-          </p>
+          </>
         </div>
         <div>
-          <span>
+          <>
             <Texto
               size={"title-s"}
               color={"--color-text"}
               label={"Curren Emissions"}
             />
-          </span>
-          <p>
             <Texto
               size={"title-ml"}
               color={"--color-titulo-lila"}
               label={"1.88/block"}
             />
-          </p>
+          </>
         </div>
       </div>
     </div>
