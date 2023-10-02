c8f3d5814b2da18fb845761ec18575b0739f7ba4
Moguete <esteveptt@gmail.com>
2023-10-02 16:29:53 +0200

    cambio params

--- src/componentes/Section/Section.js
+++ src/componentes/Section/Section.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React from "react";
 import "./Section.scss";
 
-const Section = ({className, headSVG, footSVG, headSection, bodySection, footSection}) => {
+/**
+ * @param {string} className - clase del link (primaryAnchor, secondaryAnchor, tertiaryAnchor)
+ * @param {svgImage} headSVG - SVG que llevará la sección arriba del todo
+ * @param {svgImage} footSVG - SVG que llevará la sección abajo del todo
+ * @param {HTML object} headSection - contenido del Head de la sección
+ * @param {HTML object} bodySection - contenido principal de la sección
+ *
+ * @returns Estructura HTML
+ */
+
+const Section = ({
+  className,
+  headSVG,
+  footSVG,
+  headSection,
+  bodySection,
+  footSection,
+}) => {
   return (
     <div className={className}>
-        {headSVG && headSVG};
-        {headSection && headSection};
-        {bodySection && bodySection};
-        {footSection && footSection};
-        {footSVG && footSVG};
+      {headSVG && headSVG};{headSection && headSection};
+      {bodySection && bodySection};{footSVG && footSVG};
     </div>
-  )
-}
+  );
+};
 
-export default Section
+export default Section;
