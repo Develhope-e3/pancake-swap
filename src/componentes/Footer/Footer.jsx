d5739b7ab0fb51e7dc0b6509b947f5086a3eb184
Marcoski <marcosdediego@gmail.com>
2023-10-03 12:09:01 +0200

    feat/footer

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -12,6 +12,7 @@ import Discord from "../../assets/iconos/Discord";
 import Youtube from "../../assets/iconos/YouTube";
 import Toggle from "../Button/Toggle";
 
+
 const Footer = () => {
   return (
     <footer>
@@ -178,9 +179,11 @@ const Footer = () => {
         </div>
       </div>
       {/* iconos redes */}
-
+<div className="raya-divisora-padre">
+  <div className="raya-divisora"></div>
+</div>
       <div className="parte-abajo-footer">
-        <div className="raya-divisora"></div>
+        
         {/* parte de abajo footer */}
 
         <div>
@@ -192,7 +195,17 @@ const Footer = () => {
         <div>
           <div></div>
           <div>
+            <Button
+            isPrimary={true}
+            heightValue={"32px"}
+            widthValue={"138px"}
+            texto={"Buy CAKE"}
+            svg={<svg viewBox="0 0 24 24" color="backgroundAlt" width="20px" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M5 13H16.17L11.29 17.88C10.9 18.27 10.9 18.91 11.29 19.3C11.68 19.69 12.31 19.69 12.7 19.3L19.29 12.71C19.68 12.32 19.68 11.69 19.29 11.3L12.71 4.7C12.32 4.31 11.69 4.31 11.3 4.7C10.91 5.09 10.91 5.72 11.3 6.11L16.17 11H5C4.45 11 4 11.45 4 12C4 12.55 4.45 13 5 13Z" />
+          </svg>}
             
+            />
           </div>
         </div>
       </div>
