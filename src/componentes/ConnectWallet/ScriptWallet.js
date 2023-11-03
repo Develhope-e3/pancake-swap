a3c3109d4a2bf2e3a3b83c6c56e63de97003b9e1
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-11-03 16:16:42 +0100

    fix: tip error

--- src/componentes/ConnectWallet/ScriptWallet.js
+++ src/componentes/ConnectWallet/ScriptWallet.js
@@ -7,28 +7,26 @@ export const ScriptWalletProvider = ({children}) => {
   const [wallet, setWallet] = useState(ethers.Contract);
 
   const changeWallet = () => {
-     setWallet(ethers.Wallet)
-  }
+    setWallet(ethers.Wallet);
+  };
 
   return (
     <>
-      <WalletContext.Provider value={wallet, changeWallet}>
+      <WalletContext.Provider value={{wallet, changeWallet}}>
 
-    <script type="module">ScriptWallet</script>
-    {children}
+        <script type="module">ScriptWallet</script>
+        {children}
       </WalletContext.Provider>
     </>
-  )
-  
+  );
 };
 
 export const ScriptWalletConsumer = () => {
-  const {wallet, changeWallet } = useContext();
-
+  const { wallet, changeWallet } = useContext();
 
   return (
     <>
     
     </>
-  )
-}
+  );
+};
