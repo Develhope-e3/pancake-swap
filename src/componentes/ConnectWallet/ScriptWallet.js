f203637b536a2b0f1d25ea5b045b5e00669e2857
Marcos Lambir Torres <markos_laviana@yahoo.es>
2023-11-03 15:58:37 +0100

    feat: start up of connect wallet

---
+++ src/componentes/ConnectWallet/ScriptWallet.js
@@ -0,0 +1,34 @@
+import ethers from "ethers";
+import { useContext, useState, createContext } from "react";
+
+const WalletContext = createContext(ethers);
+
+export const ScriptWalletProvider = ({children}) => {
+  const [wallet, setWallet] = useState(ethers.Contract);
+
+  const changeWallet = () => {
+     setWallet(ethers.Wallet)
+  }
+
+  return (
+    <>
+      <WalletContext.Provider value={wallet, changeWallet}>
+
+    <script type="module">ScriptWallet</script>
+    {children}
+      </WalletContext.Provider>
+    </>
+  )
+  
+};
+
+export const ScriptWalletConsumer = () => {
+  const {wallet, changeWallet } = useContext();
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
