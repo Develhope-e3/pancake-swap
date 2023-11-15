a6527eb0e6a8d246200c2724bc436e3666868f65
Barbara Contardi <barbi.contardi@gmail.com>
2023-11-15 13:04:33 +0100

    refactor: swiper

--- src/componentes/Swiper/Slides/Slide4.jsx
+++
@@ -1,9 +0,0 @@
-import asset from '../imagenes/asset 54.png'
-
-const Slide4 = () => {
-return (
-
-<img src={asset} alt="swiper" style={{width: 1110 , height : 189}}/>
-)
-}
-export default Slide4;
