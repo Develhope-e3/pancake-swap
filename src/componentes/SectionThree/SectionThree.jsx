44dcef715f1f8f59e5479b8613f4847d35beec96
Marcoski <marcosdediego@gmail.com>
2023-11-21 13:15:56 +0100

    refactor: animation section three

--- src/componentes/SectionThree/SectionThree.jsx
+++ src/componentes/SectionThree/SectionThree.jsx
@@ -1,22 +1,36 @@
 import { Subsection } from "../Subsection/Subsection";
 import SectionthreeCoins from "../../assets/imagenes/SectionthreeCoins.png";
-import "./SectionThree.scss"
+import "./SectionThree.scss";
 import { animated, useSpring } from "@react-spring/web";
 
 export function SectionThree() {
-  const { translateY } = useSpring({
-    to: { translateY: 10 }, // Cambia este valor para ajustar la altura de flotación
-    from: { translateY: 0 },
-    loop: true, // Para que la animación se repita
-    config: { tension: 5, friction: 0 },// cambiar tension para cambiar el efecto de flotar
+  const { scale } = useSpring({
+    from: { scale: 1 },
+    to: async (next) => {
+      while (true) {
+        await next({ scale: 1.1, config: { duration: 1000 } }); // Ajusta la duración aquí
+        await next({ scale: 1, config: { duration: 1000 } }); // Ajusta la duración aquí
+      }
+    },
+    reset: true,
   });
   return (
     <div className="section-three">
-      <Subsection labelTexto1={"Trade"} labelTexto2={"anything. No registration, no hassle."} labelTextoDown={"Trade any token on BNB Smart Chain in seconds, just by connnecting your wallet"}
-        labelButton={"Trade Now"} labelLink={"Learn"}
+      <Subsection
+        labelTexto1={"Trade"}
+        labelTexto2={"anything. No registration, no hassle."}
+        labelTextoDown={
+          "Trade any token on BNB Smart Chain in seconds, just by connnecting your wallet"
+        }
+        labelButton={"Trade Now"}
+        labelLink={"Learn"}
       />
       <div>
-      <animated.img style={{translateY}} src={SectionthreeCoins} alt="coins"/>
+        <animated.img
+          src={SectionthreeCoins}
+          alt="coins"
+          style={{ transform: scale.to((s) => `scale(${s})`) }}
+        />
       </div>
     </div>
   );
