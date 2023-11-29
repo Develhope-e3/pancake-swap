8b9c7585275ea83298fb93dc8dd060146dfa36d7
Barbara Contardi <barbi.contardi@gmail.com>
2023-11-29 16:55:50 +0100

    refactor:last-section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -108,7 +108,7 @@ const Home = () => {
               <Texto
                 size={"tittle-section-lg"}
                 color={"var(--color-text)"}
-                label={`Used by millions. 
+                label={`Used by millions.
                 Trusted with billions.`}
               />
               <Texto
@@ -385,55 +385,7 @@ const Home = () => {
           </Box>
         </Box>
       </Section>
-      <Section gradient={"var(--colors-gradientLastSection)"}>
-        <Box maxWidth={1168} lastSeccion={true}>
-          <Box>
-            <Box isFlexColCenter>
-              <Texto
-                size={"title-card-bold"}
-                color={"var(--text-color)"}
-                label={"Start in seconds"}
-              />
-            </Box>
-            <Box isFlexColCenter gap={1}>
-              <Texto
-                size={"title-ml"}
-                color={"var(--text-color)"}
-                label={"Start in seconds"}
-              />
-              <Texto
-                size={"title-ml-bold"}
-                color={"var(--text-color)"}
-                label={"Start in seconds"}
-              />
-            </Box>
-            <Box gap={2} isFlexColCenter>
-              <Link
-                label={"Learn how to start"}
-                className={"primary-anchor"}
-                href={""}
-                svg={
-                  <svg
-                    viewBox="0 0 24 24"
-                    width="20px"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="anchorSVG">
-                    <path d="M18 19H6C5.45 19 5 18.55 5 18V6C5 5.45 5.45 5 6 5H11C11.55 5 12 4.55 12 4C12 3.45 11.55 3 11 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V13C21 12.45 20.55 12 20 12C19.45 12 19 12.45 19 13V18C19 18.55 18.55 19 18 19ZM14 4C14 4.55 14.45 5 15 5H17.59L8.46 14.13C8.07 14.52 8.07 15.15 8.46 15.54C8.85 15.93 9.48 15.93 9.87 15.54L19 6.41V9C19 9.55 19.45 10 20 10C20.55 10 21 9.55 21 9V4C21 3.45 20.55 3 20 3H15C14.45 3 14 3.45 14 4Z"></path>
-                  </svg>
-                }
-              />
-              <Button
-                isPrimary
-                widthValue={"165.15px"}
-                heightValue={"48px"}
-                texto={"Connect Wallet"}
-                colorTexto={"var(--color-button-primary)"}
-              />
-            </Box>
-          </Box>
-          <LastSectionSVG />
-        </Box>
-      </Section>
+      <LastSectionSVG />
       <SlideBunny />
       <ButtonScroll />
     </BasePage>
